9ec108a33076f01beafb07074e88314b524da6af
Krayadev <fabian.schwickert@me.com>
2020-10-19 11:17:22 +0200

    Added some elementary components.

---
+++ frontend/src/app/MainPageSections/shorten-section/shorten-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shorten-section',
+  templateUrl: './shorten-section.component.html',
+  styleUrls: ['./shorten-section.component.css']
+})
+export class ShortenSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
