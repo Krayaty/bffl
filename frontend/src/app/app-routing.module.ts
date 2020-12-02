c24481f9b37c509e8e59a1212175f446f650d7fc
bastis007 <schaefer.bastian@mail.de>
2020-12-02 19:39:09 +0100

    Reorganize menubar for Angular and make it work

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './StartUpComponents/login/login.component';
 import {MainPageComponent} from './MainPageSections/main-page/main-page.component';
-
+import {ShortenSectionComponent} from './MainPageSections/shorten-section/shorten-section.component';
+import {UrlManagerSectionComponent} from './MainPageSections/url-manager-section/url-manager-section.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'main', component: MainPageComponent },
+  { path: 'shorten', component: ShortenSectionComponent },
+  { path: 'manager', component: UrlManagerSectionComponent },
   { path: '',   redirectTo: '/main', pathMatch: 'full' },
   { path: '**', component: MainPageComponent }
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+//import { TopBarComponent } from './top-bar/top-bar.component';
 import { ShortenSectionComponent } from './MainPageSections/shorten-section/shorten-section.component';
 import { UrlManagerSectionComponent } from './MainPageSections/url-manager-section/url-manager-section.component';
 import { FormsModule } from '@angular/forms';
