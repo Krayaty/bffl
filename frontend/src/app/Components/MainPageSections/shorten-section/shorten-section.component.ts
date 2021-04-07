9fc3082150dbeef550e2d2b390175dae202112c9
bastis007 <schaefer.bastian@mail.de>
2021-04-07 13:53:28 +0200

    Wrote functions to shorten the URL, call from HTML does not work somehow. Functions are not final, yet. Connection to database is still missing

--- frontend/src/app/Components/MainPageSections/shorten-section/shorten-section.component.ts
+++ frontend/src/app/Components/MainPageSections/shorten-section/shorten-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DbConnectorService} from '../../../Services/DB-Connect-Services/db-connector.service';
 
 @Component({
   selector: 'app-shorten-section',
@@ -6,10 +7,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shorten-section.component.css']
 })
 export class ShortenSectionComponent implements OnInit {
+  document: any;
 
-  constructor() { }
+  constructor(private dbconnector: DbConnectorService) { }
 
   ngOnInit(): void {
   }
 
+  ShortenURL(url: string, wishURL: string): string {
+    if (URL_In_Database(wishURL) === true) {
+      url = wishURL;
+      SaveURL(url);
+    }
+    else {
+      const additionToURL = String(CreateRandomChar());
+      wishURL = wishURL + additionToURL;
+      ShortenURL(wishURL);
+    }
+    document.getElementById('resultURL').innerHTML = url.toString();
+    return url;
+  }
+
+}
+
+function ShortenURL(wishURL: string): string {
+  let url: string;
+  if (URL_In_Database(wishURL) === true) {
+    url = wishURL;
+    SaveURL(url);
+  }
+  else {
+    const additionToURL = String(CreateRandomChar());
+    wishURL = wishURL + additionToURL;
+    ShortenURL(wishURL);
+  }
+  document.getElementById('resultURL').innerHTML = url;
+  return url;
+}
+
+function URL_In_Database(wishURL: string): boolean {
+  // this.dbconnector.getAllTargetURLs();
+  return true;
+}
+
+function CreateRandomChar(): string {
+  let max = 3;
+  let min = 1;
+  let randomNumber: number = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  switch (randomNumber) {
+    case(1):
+      max = 57;
+      min = 48;
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      let res = String.fromCharCode(randomNumber);
+      return res;
+    case(2):
+      max = 90;
+      min = 65;
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      res = String.fromCharCode(randomNumber);
+      return res;
+    case(3):
+      max = 122;
+      min = 97;
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      res = String.fromCharCode(randomNumber);
+      return res;
+  }
+}
+
+function SaveURL(url: string): boolean{
+  // this.dbconnector.save(url); ?
+  return true;
 }
