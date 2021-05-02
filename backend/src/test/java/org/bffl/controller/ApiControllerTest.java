7889be912d3ba801f77ecbbce0ef468f565f1b0c
felixhir <felix.genie@gmail.com>
2021-05-02 11:39:35 +0200

    test: moved ApiControllerTest.java into test-dir. Added multiple dependencies and plugins (site, surefire[-reports]), slightly adjusted DBConnector.run.xml to now refer the actual Start class

--- backend/src/test/java/org/bffl/controller/ApiControllerTest.java
+++ backend/src/test/java/org/bffl/controller/ApiControllerTest.java
@@ -2,7 +2,7 @@ package org.bffl.controller;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ApiControllerTest {
 
