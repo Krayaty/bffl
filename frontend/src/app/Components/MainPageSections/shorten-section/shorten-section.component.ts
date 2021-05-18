8728aadb834950dd06db91e75427c94cdc158941
Bastian Schäfer <schaefer.bastian@mail.de>
2021-05-18 23:36:45 +0200

    Complete control over the user input. Wrong or missing arguments are rejected with hints.

--- frontend/src/app/Components/MainPageSections/shorten-section/shorten-section.component.ts
+++ frontend/src/app/Components/MainPageSections/shorten-section/shorten-section.component.ts
@@ -1,41 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { DbConnectorService } from '../../../Services/DB-Connect-Services/db-connector.service';
-import { AuthService } from '../../../Services/Iam-Services/auth.service';
-import { FormBuilder } from '@angular/forms';
-import {Validators} from '@angular/forms';
-import { ShortenService } from '../../../Services/Shorten-Services/shorten.service';
-
+import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {DbConnectorService} from '../../../Services/DB-Connect-Services/db-connector.service';
+import {AuthService} from '../../../Services/Iam-Services/auth.service';
+import {ShortenService} from '../../../Services/Shorten-Services/shorten.service';
 
 @Component({
   selector: 'app-shorten-section',
   templateUrl: './shorten-section.component.html',
   styleUrls: ['./shorten-section.component.css']
 })
-export class ShortenSectionComponent implements OnInit {
+
+export class ShortenSectionComponent {
+
+  shortenURLForm: FormGroup;
+
+  originalUrlRegEx = '[-a-zA-Z0-9@:%._\\+~#=\/\/]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&\\/\\/=]*)';
+  customSuffixRegEx = '[a-zA-Z0-9]+[\.a-zA-Z0-9]*';
 
   items = this.shortenService.getItems();
-
-  shortenURLForm = this.formBuilder.group({
-    targetURL: ['', Validators.required],
-    customSuffix: ['', Validators.required],
-    updateFlag: '',
-    deleteFlag: '',
-    scope: '',
-    tags: ''
-  });
 
   constructor(private dbconnector: DbConnectorService,
               private authService: AuthService,
               private formBuilder: FormBuilder,
               private shortenService: ShortenService) {
+    this.shortenURLForm = new FormGroup({
+      originalURL: new FormControl('', {
+        validators: [Validators.required, Validators.pattern(this.originalUrlRegEx)]}),
+      customSuffix: new FormControl('', {
+        validators: [Validators.required, Validators.pattern(this.customSuffixRegEx)]}),
+      updateFlag: new FormControl('', {}),
+      deleteFlag: new FormControl('', {}),
+      scope: new FormControl('', {
+        validators: [Validators.required]}),
+      tags: new FormControl('', {}),
+    });
   }
 
-  ngOnInit(): void {}
-
-  shortenURL(): void {
+  shortenURL(): boolean {
+    window.alert('shortenURL-Method');
     const assignedTagIds: number[] = [];
     if (this.shortenURLForm.get('tags').value != null) {
-      assignedTagIds.push();
+         assignedTagIds.push();
+    }
+
+    if ( this.shortenURLForm.get('originalURL').value == null || this.shortenURLForm.get('originalURL').value === '' ) {
+         window.alert('Missing or wrong argument in original URL');
+         return false;
+    }
+    if ( this.shortenURLForm.get('customSuffix').value == null || this.shortenURLForm.get('customSuffix').value === '') {
+        window.alert('Missing or wrong argument in short URL');
+        return false;
+    }
+    if ( this.shortenURLForm.get('scope').value == null || this.shortenURLForm.get('scope').value === '' ) {
+        window.alert('Missing or wrong argument in scope');
+        return false;
     }
 
     this.dbconnector.saveNewShortURLWithTags(
@@ -43,9 +61,10 @@ export class ShortenSectionComponent implements OnInit {
       this.shortenURLForm.get('scope').value,
       this.shortenURLForm.get('deleteFlag').value,
       this.shortenURLForm.get('updateFlag').value,
-      this.shortenURLForm.get('targetURL').value,
-      assignedTagIds
+      this.shortenURLForm.get('originalURL').value,
+      assignedTagIds,
     );
     this.shortenURLForm.reset();
+    return true;
   }
 }
