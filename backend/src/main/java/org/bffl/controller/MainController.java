4e6b211c5dae209d588051ab897f45cb2cc0f3f3
hudla <hudla01@gmx.de>
2021-06-14 14:07:35 +0200

    removed debugging

--- backend/src/main/java/org/bffl/controller/MainController.java
+++ backend/src/main/java/org/bffl/controller/MainController.java
@@ -170,8 +170,7 @@ public class MainController {
         }
 
         for(int tagId: assignedTagIds){
-            String suffix = body.getCustom_suffix();
-            if(this.url_has_tagRepo.saveTagOfGroupToShortURLBySuffix(tagId, body.getGroup_name(), suffix) != 1) {
+            if(this.url_has_tagRepo.saveTagOfGroupToShortURLBySuffix(tagId, body.getGroup_name(), body.getCustom_suffix()) != 1) {
                 return HttpStatus.BAD_REQUEST.value();
             }
         }
